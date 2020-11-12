87e9e7e59f269a1c6f8ee33efc0557927e5724ba
mdelef00 <mdelef00@estudiantes.unileon.es>
2020-11-12 13:18:41 +0100

    Creacion de ficheros y carpetas iniciales

---
+++ date/src/es/unileon/prg1/date/Date.java
@@ -0,0 +1,16 @@
+package es.unileon.prg1.date;
+
+public class Date{
+
+    private int day;
+
+    private int month;
+
+    private int year;
+
+    public Date(){
+        this.day = 1;
+        this.month = 1;
+        this.year = 2020;
+    }
+}
