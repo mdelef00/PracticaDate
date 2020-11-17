de6b41fb4a8985de0782a5a3a78a5e7f7ea59a79
mdelef00 <mdelef00@estudiantes.unileon.es>
2020-11-17 19:00:24 +0100

    Mejora del build y continuacion clase date

--- date/src/es/unileon/prg1/date/Date.java
+++ date/src/es/unileon/prg1/date/Date.java
@@ -13,4 +13,31 @@ public class Date{
         this.month = 1;
         this.year = 2020;
     }
+
+    public Date(int date, int month, int year){
+        setDay(day);
+        setMonth(month);
+        setYear(year);
+    }
+
+    public int getDay(){
+        return day;
+    }
+    public void setDay(int day){
+        this.day = day;
+    }
+
+    public int getMonth(){
+        return month;
+    }
+    public void setMonth(int month){
+        this.month = month;
+    }
+
+    public int getYear(){
+        return year;
+    }
+    public void setYear(int year){
+        this.year = year;
+    }
 }
